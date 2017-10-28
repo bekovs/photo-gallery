ca36e66a1d6c2e644bdc1472bab53ed9506b2b70
Unknown <bekovs96@gmail.com>
2017-10-28 13:40:08 +0600

    Some Active Admin changes added
    Also bootstrap theme added

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Post do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :title, :image, :user_id
 #
 # or
 #
@@ -11,5 +11,40 @@ ActiveAdmin.register Post do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
+	form do |f|
+    f.inputs do
+      f.input :title
+      f.input :image, :as => :file
+      f.input :user
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :image do |post|
+      image_tag post.image.url(:small)
+    end
+    column :title do |post|
+      link_to post.title, admin_post_path(post)
+    end
+    column :user
+    actions
+  end
+
+  show do
+	 attributes_table do
+	   row :image do |post|
+	     image_tag post.image.url(:medium)
+	   end
+	   row :title
+	   row :user
+	 end
+	 active_admin_comments
+	end
+
+
 
 end
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :name, :email, :password, :password_confirmation, :admin
 #
 # or
 #
@@ -11,5 +11,38 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
+	form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :admin
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :name do |user|
+      link_to user.name, admin_user_path(user)
+    end
+    column :email
+    column :admin
+    actions
+  end
+
+  show do
+	 attributes_table do
+	   row :name
+	   row :email
+	   row :admin
+	 end
+	 active_admin_comments
+	end
+
+
 
 end
