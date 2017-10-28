25e2c347c0879688b5abf7f6d929cd28b2373c8a
Unknown <bekovs96@gmail.com>
2017-10-28 12:31:59 +0600

    Devise views normalized. Added some user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin_user = User.create(name: 'Admin', email: 'admin@gmail.com', password: 'adminadmin', password_confirmation: 'adminadmin', admin: true)
+
+usrs = User.create([{ name: 'Peter', email: 'peter@gmail.com', password: 'qwerty', password_confirmation: 'qwerty' }, 
+										{ name: 'John', email: 'john@gmail.com', password: 'qwerty', password_confirmation: 'qwerty' },
+										{ name: 'Clara', email: 'clara@gmail.com', password: 'qwerty', password_confirmation: 'qwerty' },
+										{ name: 'Jenny', email: 'jenny@gmail.com', password: 'qwerty', password_confirmation: 'qwerty' },
+										{ name: 'Amanda', email: 'amanda@gmail.com', password: 'qwerty', password_confirmation: 'qwerty' }])
